5e74248c05c6553811225495d39454ac029141aa
Stick-To <34546552+Stick-To@users.noreply.github.com>
2018-10-03 19:23:24 +0800

    update

--- convert_to_tfrecord.py
+++ convert_to_tfrecord.py
@@ -3,30 +3,34 @@ from __future__ import division
 from __future__ import print_function
 import tensorflow as tf
 import numpy as np
-import glob
 import cv2
-
-#create tfrecord
-#default:img_name[-5] is the label (number),convert to one_hot<----(zeors(3,np.int64),in this example)
+import os
+import json
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
-def convert_to_tfrecord(imgdir, filename ,newshape=(160,90)):
-    image_list= glob.glob(imgdir+"*.jpg")
+def convert_to_tfrecord(imgdir, json_file, filename ,num_classes, newshape=(160,90)):
+    f = open(json_file)
+    decoder = json.load(f)
     tf.reset_default_graph()
     print('Writing', filename)
     tensor = tf.placeholder(tf.uint8)
     encode_jpeg = tf.image.encode_jpeg(tensor)
     session = tf.Session()
     with tf.python_io.TFRecordWriter(filename) as writer:
-        for index in range(len(image_list)):
-            img_name = image_list[index]
+        i=0
+        for entry in decoder:
+            img_name = os.path.join(imgdir, entry['image_id'])
             print('Writing img',img_name,'now')
-            label_ = int(img_name[-5])
-            label = np.zeros(3,dtype=np.int64)
+            label_ = int(entry['disease_class'])
+            label = np.zeros(num_classes,dtype=np.int64)
+            label[label_] = 1
+            print(label)
+            i+=1
+            print('img num',i)
             label = label.tostring()
-            image_raw = cv2.imread(img_name)
+            image_raw = cv2.imdecode(np.fromfile(img_name,dtype=np.uint8),-1)
             image_raw = cv2.resize(image_raw, newshape)
             shape = np.array(image_raw.shape, np.int32).tobytes()
             jpeg_bytes = session.run(encode_jpeg, feed_dict={tensor: image_raw})
