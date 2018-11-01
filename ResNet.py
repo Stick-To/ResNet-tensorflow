251a68a3e83d30ef57611aecb6a5219e8aae5793
xinru <34546552+Stick-To@users.noreply.github.com>
2018-11-01 21:17:53 +0800

    update

--- ResNet.py
+++ ResNet.py
@@ -2,54 +2,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import tensorflow as tf
-import os
-import numpy as np
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 #ResNet-v2
 class ResNet:
-    def __init__(self,dataprovider_train,dataprovider_test,init_learning_rate,l2_regularization_weight,epochs,
-                 init_conv_param,init_pool_param,blocks_list,data_format,is_bottleneck):
+    def __init__(self,data_shape,num_classes,init_conv_param,init_pool_param,
+                 blocks_list,data_format,is_bottleneck):
 
-        """
-            dataprovider_train:class dataprovider,trainset inside
-            dataprovider_test:class dataprovier, if not None test it every epoch during train process
-                                        and  default be used  in test() function
-            init_learning_rate:the learning_rate when train process start
-            l2_regularization_weight:weight_delay for l2_loss
-            epochs:epochs
-            init_conv_param:dict, the parameters of init_conv  inside
-            init_pool_param:dict, the parameters of init_pool  inside
-            block_list:list,the times of every filters(the filters parameter of conv2d in tf) to be use,the first block use the
-                            filters of init_conv and double it after it is used the times that this list claim
-            data_format:'channels_first' or 'channels_last'
-            is_bottleneck:Whether use bottleneck block
-        """
-
-        self.dataprovider_train = dataprovider_train
-        self.data_shape = dataprovider_train.data_shape
-        self.num_classes = dataprovider_train.num_classes
-        self.train_initop = dataprovider_train.initop
-        self.train_iterator = dataprovider_train.iterator
-        self.num_train = dataprovider_train.num_samples
-        self.train_batch_size = dataprovider_train.batch_size
-        self.train_labels,_ ,self.train_images = self.train_iterator.get_next()
-        self.train_images =tf.cast(self.train_images,tf.float32)
-
-        self.is_test = True if dataprovider_test is not None else False
-        if self.is_test:
-            self.dataprovider_test = dataprovider_test
-            self.test_initop = dataprovider_test.initop
-            self.test_iterator = dataprovider_test.iterator
-            self.num_test = dataprovider_test.num_samples
-            self.test_batch_size = dataprovider_test.batch_size
-            self.test_labels,_ ,self.test_images = self.test_iterator.get_next()
-            self.test_images =tf.cast(self.test_images,tf.float32)
-
-
-
-        self.learning_rate = init_learning_rate
-        self.l2_regularization_loss = l2_regularization_weight
-        self.epochs = epochs
+        self.data_shape = data_shape
+        self.num_classes = num_classes
         self.init_conv_param = init_conv_param
         self.init_pool_param = init_pool_param
         self.blocks_list = blocks_list
@@ -57,8 +16,7 @@ class ResNet:
         self.is_training = True
         self.is_bottleneck = is_bottleneck
         self.filters = [self.init_conv_param['filters']*(2**i) for i in range(len(blocks_list))]
-        self.global_step = tf.get_variable(name='global_step',initializer=tf.constant(0,dtype=tf.int32),
-                                           trainable=False)
+        self.global_step = tf.train.get_or_create_global_step()
 
         self._define_inputs()
         self._build_graph()
@@ -68,26 +26,29 @@ class ResNet:
         shape.extend(self.data_shape)
         self.images = tf.placeholder(dtype=tf.float32,shape=shape,name='images')
         self.labels = tf.placeholder(dtype=tf.int32,shape=[None,self.num_classes],name='labels')
+        self.learning_rate = tf.placeholder(dtype=tf.float32,shape=[],name='lr')
+        self.l2_rate = tf.placeholder(dtype=tf.float32,shape=[],name='l2_rate')
+        self.prob = tf.placeholder(dtype=tf.float32,shape=[],name='prob')
     def conv_layer(self, bottom, kernel_size, filters, strides=1, name='conv'):
-        #pad = kernel_size // 2
-        #if self.data_format == 'channels_last':
-        #    bottom_pad = tf.pad(bottom, [[0,0],[pad,pad],[pad,pad],[0,0]])
-        #else:
-        #    bottom_pad = tf.pad(bottom, [[0,0],[0,0],[pad,pad],[pad,pad]])
         return tf.layers.conv2d(
             inputs=bottom,
             filters=filters,
             kernel_size = [kernel_size,kernel_size],
             strides=[strides,strides],
-            padding='SAME',
-            use_bias = True,
+            padding='same',
             name = name,
             data_format = self.data_format
         )
     def max_pool(self, bottom, pool_size=2, strides=2):
-        return tf.layers.max_pooling2d(bottom, [pool_size,pool_size], [strides,strides],data_format=self.data_format,padding='SAME')
+        return tf.layers.max_pooling2d(bottom,
+                [pool_size,pool_size],
+                [strides,strides],
+                data_format=self.data_format,
+                padding='same'
+        )
     def batch_norm(self,bottom):
-        return tf.layers.batch_normalization(bottom,axis=3 if self.data_format=='channels_last' else 1,
+        axes = 3 if self.data_format=='channels_last' else 1
+        return tf.layers.batch_normalization(bottom,axis=axes,
                     training = self.is_training)
     def project_shortcut(self, bottom, filters, is_shutcut_pooling=True):
         strides = 2 if is_shutcut_pooling else 1
@@ -145,7 +106,7 @@ class ResNet:
                                          self.init_conv_param['filters'],
                                          strides=self.init_conv_param['strides'],
                                          name = 'init_conv')
-        self.init_pool = self.max_pool(self.init_conv,self.init_pool_param['pool_size'],
+        self.init_pool = self.max_pool(self.init_conv,self.init_pool_param['pooling_size'],
                                        self.init_pool_param['strides'])
 
         if self.is_bottleneck:
@@ -159,10 +120,9 @@ class ResNet:
             for i in range(1, len(self.blocks_list)):
                 residual_block = stack_block_fn(residual_block, self.filters[i] ,'residual_block_'+str(i+1),self.blocks_list[i], True)
 
-        axes = [2,3] if self.data_format=='channels_last' else [1,2]
+        axes = [1,2] if self.data_format=='channels_last' else [2,3]
 
         with tf.variable_scope('final_layers'):
-            #use tf.reduce_mean instead of average_pool like tensorflow.models.offcial.resnet
             self.final_average_pool = tf.reduce_mean(residual_block,
                                                  axes,
                                                  keepdims=False,
@@ -173,7 +133,7 @@ class ResNet:
         with tf.variable_scope('result'):
             self.logits = tf.nn.softmax(self.final_dense)
             self.softmax_loss = tf.losses.softmax_cross_entropy(self.labels,self.final_dense,reduction=tf.losses.Reduction.MEAN)
-            self.l2_loss = self.l2_regularization_loss * tf.add_n(
+            self.l2_loss = self.l2_rate * tf.add_n(
                         [tf.nn.l2_loss(var) for var in tf.trainable_variables()])
             self.loss = self.softmax_loss + self.l2_loss
             self.pred = tf.argmax(self.logits, 1)
@@ -186,49 +146,29 @@ class ResNet:
     def _init_session(self):
         self.sess = tf.InteractiveSession()
         self.sess.run(tf.global_variables_initializer())
-    def train_one_epoch(self):
+    def train_one_epoch(self, images, labels, lr, l2_rate, prob):
         self.is_training = True
-        total_loss = []
-        total_acc = []
-        self.sess.run(self.train_initop)
-        for i in range(self.num_train // self.train_batch_size):
-            images, labels = self.sess.run([self.train_images,self.train_labels])
-            loss,acc, _ = self.sess.run([self.loss,self.accuracy,self.optimizer],
-                                        feed_dict={
-                                            self.images:images,
-                                            self.labels:labels
-                                        })
-            total_loss.append(loss)
-            total_acc.append(acc)
-        mean_loss = np.mean(total_loss)
-        mean_acc = np.mean(total_acc)
-        return mean_loss, mean_acc
-
-    def test(self):
+        loss,acc, _ = self.sess.run([self.loss,self.accuracy,self.optimizer],
+                                    feed_dict={
+                                        self.images:images,
+                                        self.labels:labels,
+                                        self.learning_rate:lr,
+                                        self.l2_rate:l2_rate,
+                                        self.prob:prob
+                                    })
+        return loss, acc
+    def validate_one_epoch(self, images, labels, l2_rate, prob):
         self.is_training = False
-        total_loss = []
-        total_acc = []
-        self.sess.run(self.test_initop)
-        for i in range(self.num_test // self.test_batch_size):
-            images, labels = self.sess.run([self.test_images,self.test_labels])
-            loss,acc = self.sess.run([self.loss,self.accuracy],
-                                     feed_dict={
-                                         self.images:images,
-                                         self.labels:labels
-                                     })
-            total_loss.append(loss)
-            total_acc.append(acc)
-        mean_loss = np.mean(total_loss)
-        mean_acc = np.mean(total_acc)
-        return mean_loss, mean_acc
+        loss, acc = self.sess.run([self.loss,self.accuracy],
+                                    feed_dict={
+                                        self.images:images,
+                                        self.labels:labels,
+                                        self.learning_rate:0.,
+                                        self.l2_rate:l2_rate,
+                                        self.prob:prob
+                                    })
+        return loss, acc
 
-    def train_all_epochs(self):
 
-        for epoch in range(self.epochs):
-            print('-'*25,'epoch: ',epoch,'-'*25)
-            loss, acc = self.train_one_epoch()
-            print('train opech: ',epoch,' mean loss: ',loss," mean acc: ",acc)
-            if self.dataprovider_test is not None:
-                loss, acc = self.test()
-                print('val opech: ',epoch,' mean loss: ',loss," mean acc: ",acc)
+
 
--- test.py
+++ test.py
@@ -1,10 +1,43 @@
-import ResNet as net
-from dataprovider import dataprovider
 
-train_set = [".\\tfrecord\\train.tfrecord"]
-val_set = [".\\tfrecord\\val.tfrecord"]
-trainset = dataprovider(train_set,3,1037,[90,160,3],1037,2)
-valset = dataprovider(val_set,3,111,[90,160,3],111,2)
+import tensorflow as tf
+import keras
+from keras.datasets import cifar10
+from keras.datasets import cifar100
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+import os
+import sys
+os.environ['TF_CPP_MIN_LOG_LEVEL'] ='2'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+import ResNet as net
+
+device_name = tf.test.gpu_device_name()
+if device_name is not '':
+    print('Found GPU Device!')
+else:
+    print('Found GPU Device Failed!')
+
+
+data_shape = (32,32,3)
+num_train = 50000
+num_test = 10000
+num_classes = 10
+batch_size = 32
+epochs = 200
+
+(x_train, y_train),(x_test, y_test) = cifar10.load_data()
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+train_datagen = ImageDataGenerator(
+    horizontal_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.1,
+    zoom_range=0.1,
+    rotation_range=30.
+).flow(x_train, y_train, batch_size=batch_size)
+test_datagen = ImageDataGenerator().flow(x_test, y_test, batch_size=batch_size)
+
 
 init_conv_param = {
     'kernel_size':7,
@@ -12,9 +45,38 @@ init_conv_param = {
     'strides':2
 }
 init_pool_param = {
-    'pool_size':3,
+    'pooling_size':3,
     'strides':2
 }
-#init_conv—init_pool—[64 64 64,128 128 128,256 256 256]—averge_pool——dense_layer--->[3,4,3]
-testnet = net.ResNet(trainset,valset,0.01,1e-4,400,init_conv_param,init_pool_param,[3,4,3],'channels_last',False)
-testnet.train_all_epochs()
+
+lr = 0.001
+l2_rate = 1e-4
+drop_prob = 0.3
+testnet = net.ResNet((32,32,3),10,init_conv_param,init_pool_param,[3,4,6,3],'channels_last',True)
+for epoch in range(epochs):
+    print('-'*50,'epoch',epoch,'-'*50)
+    train_acc = []
+    train_loss = []
+    val_acc = []
+    val_loss = []
+    for iter in range(num_train//batch_size):
+        images, labels = train_datagen.next()
+        loss, acc = testnet.train_one_epoch(images, labels, lr, l2_rate, drop_prob)
+        sys.stdout.write('\r>> train iter '+str(iter)+' loss '+str(loss)+' acc '+str(acc))
+        train_acc.append(acc)
+        train_loss.append(loss)
+    train_mean_loss = np.mean(train_loss)
+    train_mean_acc = np.mean(train_acc)
+    sys.stdout.write('\n')
+    print('>> train mean loss',train_mean_loss,' train mean acc:',train_mean_acc)
+    for iter in range(num_test//batch_size):
+        images, labels = test_datagen.next()
+        loss, acc = testnet.train_one_epoch(images, labels, lr, l2_rate, drop_prob)
+        sys.stdout.write('\r>> test iter '+str(iter)+' loss '+str(loss)+' acc '+str(acc))
+        train_acc.append(acc)
+        train_loss.append(loss)
+    train_mean_loss = np.mean(train_loss)
+    train_mean_acc = np.mean(train_acc)
+    sys.stdout.write('\n')
+    print('>> test mean loss',train_mean_loss,' test mean acc:',train_mean_acc)
+
